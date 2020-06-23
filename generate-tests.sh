12acff8be138b79dae94131590620b6be4ed9162
Joshua Li <josh@jrl.ninja>
2020-06-23 00:47:28 -0700

    test: generate a massive unified diff from my dotfiles

--- generate-tests.sh
+++ generate-tests.sh
@@ -7,13 +7,22 @@ mkdir -p "${HERE}/tests"
 cd "$1"
 [[ ! -d ".git" ]] && {
     echo "${1} doesn't seem to be a git repository."
-    echo "usage: ${0} path/to/git/repo"
+    echo "usage: ${0} path/to/git/repo [# commits from head]"
     exit 1
 }
 
 d="${HERE}/tests/$(basename $PWD)"
 mkdir -p "$d"
-for i in {1..10}; do
+
+if [[ -z "$2" ]]; then
+    read -n1 -p "You're about to generate a massive test from repo ${1}. Are you sure? (ENTER) "
+    mkdir "${d}/1"
+    git log --patch > "${d}/1/in.diff"
+    YDIFF_WIDTH=130 "${HERE}/ydiff" -s < "${d}/1/in.diff" > "${d}/1/out"
+    exit
+fi
+
+for i in $(seq 1 "$2"); do
     mkdir -v "${d}/${i}"
     git show --no-ext-diff "HEAD~${i}" > "${d}/${i}/in.diff"
     YDIFF_WIDTH=130 "${HERE}/ydiff" -s < "${d}/${i}/in.diff" > "${d}/${i}/out"
